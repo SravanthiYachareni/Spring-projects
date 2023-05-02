6e78f3afc0de65445aa7582213450e094a503e8c
Sravanthi Yachareni <sravanthiyachareni@gmail.com>
2023-05-02 14:52:03 +0530

    added

---
+++ Firstapplication/src/main/java/com/example/demo/FirstapplicationApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstapplicationApplication.class, args);
+		System.out.println("Hello Spring");
+	}
+
+}
